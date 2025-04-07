85813047a1eaba66c993ae7ea6f368d456acc32b
Josip2504 <josip.samardzic2@gmail.com>
2025-04-07 13:06:41 -0400

    passowrd

--- srcs/requirements/mariadb/tools/configure.sh
+++ srcs/requirements/mariadb/tools/configure.sh
@@ -29,11 +29,11 @@ cat /tmp/mysql_debug.log || echo "No debug log found."
 : "${MYSQL_USER:=${MYSQL_USER_FILE:+$(cat $MYSQL_USER_FILE)}}"
 : "${MYSQL_DATABASE:=wordpress}"
 
-mysql --socket=/var/run/mysqld/mysqld.sock <<-EOSQL
+mysql -u root -p"${MYSQL_ROOT_PASSWORD}" --socket=/var/run/mysqld/mysqld.sock <<-EOSQL
     SET @@SESSION.SQL_LOG_BIN=0;
-    DELETE FROM mysql.user WHERE user NOT IN ('mysql.sys', 'mysqlxsys', 'root') OR host NOT IN ('localhost');
-    CREATE USER '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';
-    GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE}.* TO '${MYSQL_USER}'@'%';
+    CREATE DATABASE IF NOT EXISTS \`${MYSQL_DATABASE}\`;
+    CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';
+    GRANT ALL PRIVILEGES ON \`${MYSQL_DATABASE}\`.* TO '${MYSQL_USER}'@'%';
     ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';
     FLUSH PRIVILEGES;
 EOSQL
